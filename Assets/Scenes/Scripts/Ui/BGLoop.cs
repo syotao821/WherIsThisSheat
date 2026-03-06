f42b3f5bf659d713a0b730af84943a40308fb64b
imachi00 <149231271+imachi00@users.noreply.github.com>
2026-03-06 15:14:44 +0900

    セレクトシーンほぼ完了

---
+++ Assets/Scenes/Scripts/Ui/BGLoop.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BGLoop : MonoBehaviour
+{
+	private const float MAX_OFFSET = 1f;
+	private const string PROPERTY_NAME = "_MainTex";
+
+	Vector2 _offsetSpeed = new Vector2(0.1f,0.1f);
+	Material _material;
+
+	private void Start()
+	{
+		_material = GetComponent<Image>().material;
+
+		InvokeRepeating(nameof(MoveUpdate), 0f, 0.01f);
+	}
+
+	private void MoveUpdate()
+	{
+		if (_material != null)
+		{
+			var x = Mathf.Repeat(Time.time * _offsetSpeed.x, MAX_OFFSET);
+			var y = Mathf.Repeat(Time.time * _offsetSpeed.y, MAX_OFFSET);
+			var offset = new Vector2(x, y);
+			_material.SetTextureOffset(PROPERTY_NAME, offset);
+		}
+	}
+
+	private void OnDestroy()
+	{
+		// オブジェクトが破棄されるタイミングに位置をリセットする
+		if (_material != null)
+		{
+			_material.SetTextureOffset(PROPERTY_NAME, Vector2.zero);
+		}
+	}
+}
